f9f2ba4c8d71b824a5fb83e93dfdda84add990ea
Pablo Rincon <coljung.work@gmail.com>
2020-12-22 23:53:01 -0500

    add dummy todo test file

---
+++ src/reviews-module/ReviewsModule.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import ReviewsModule from "./ReviewsModule";
+
+//TODO
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
